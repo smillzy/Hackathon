7ae3bc91a50b187bf84436879f8f7287b8d774fe
phoebe <mongzhen0856@gmail.com>
2026-05-26 10:45:49 +0800

    加入專案檔案。

---
+++ contentReplacement/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace contentReplacement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個整數");
+            int max;
+
+            while (!int.TryParse(Console.ReadLine(), out max) || max <= 90)
+            {
+                Console.WriteLine("請輸入正確的整數，且必須大於90");
+            }
+
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) // 最嚴格的條件放最上面
+                {
+                    Console.WriteLine("Dann");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
